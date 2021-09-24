d72bc2554529b6aa0dc9751ef3d2e539f988eff2
Yo_4040 <51785135+Yo4040@users.noreply.github.com>
2021-09-25 01:33:45 +0900

    Add files via upload

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ const line = require('@line/bot-sdk');
 const PORT = process.env.PORT || 3000;
 
 const config = {
-    channelAccessToken: '',
-    channelSecret: ''
+    channelAccessToken: '4rK2NXLs39EbTh6nXbaupT3B+ZPhB+2/z/hdp0deLA+kJWmb4nvep75QfbD4O4P4zRe1SI+7EIkZyZ7LnFjpBjNb1/hUdVwx1jNuR42ow7FKk9QtEkODQDFI4kd6NwGeN7VPjS4O9WwYmYFp0tY3eAdB04t89/1O/w1cDnyilFU=',
+    channelSecret: '8c59ddf565d45b68dec7839ec63f6885'
 };
 
 const app = express();
